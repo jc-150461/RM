da9024dac3c33ccb8795959f6b956f057bcd3fc7
燎 石橋 <150461@jc-21.jp>
2017-11-28 12:52:38 +0900

    ver

--- RM/RM/MainPage.xaml.cs
+++ RM/RM/MainPage.xaml.cs
@@ -18,7 +18,6 @@ namespace RM
             Double w = Double.Parse(Weight.Text);
             Double l = Double.Parse(Leg.Text);
             Double RM = 0.0;
-
             if (l = 1)
             {
                 RM = w / 1;
