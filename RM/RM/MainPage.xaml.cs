b4126ac0a711934fdc858344782b2e834a2db2bf
燎 石橋 <150461@jc-21.jp>
2017-11-28 16:10:27 +0900

    RM計算

--- RM/RM/MainPage.xaml.cs
+++ RM/RM/MainPage.xaml.cs
@@ -17,12 +17,31 @@ namespace RM
         {
             Double w = Double.Parse(Weight.Text);
             Double l = Double.Parse(Leg.Text);
-            Double RM = 0.0;
-            if (l == 1.0)
-            {
+            Double RM1 = 0.0;
+            Double RM2 = 0.0;
+            Double RM3 = 0.0;
+            Double RM4 = 0.0;
+            Double RM5 = 0.0;
+            Double RM6 = 0.0;
+            Double RM7 = 0.0;
+            Double RM8 = 0.0;
+            Double RM9 = 0.0;
+            Double RM10 = 0.0;
+            
                 RM = w / 1.0;
-            }
-            DisplayAlert("RM", RM.ToString(), "OK");
+                RM2 = w / 0.95;
+                RM3 = w / 0.93;
+                RM4 = w / 0.90;
+                RM5 = w / 0.87;
+                RM6 = w / 0.85;
+                RM7 = w / 0.87;
+                RM8 = w / 0.80;
+                RM9 = w / 0.77;
+                RM10 = w / 0.75;
+
+            LabelText.Text = RM1;
+
+            
         }
 
 	}
